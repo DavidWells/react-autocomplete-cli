bf773c7086d212c66e630ebb6894e8ce5fc399d7
davidwells <david@inboundnow.com>
2016-07-07 18:11:33 -0700

    init commit

---
+++ generate-completions.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var fs = require('fs')
+var path = require('path')
+var argv = require('yargs').argv
+var exec = require('child_process').exec
+var generateCompletions = require('./parse-docgen')
+var cwd = process.cwd()
+var importJSConfigPath = path.join(cwd, '.importjs.json')
+var docGenPath = path.join(cwd, 'node_modules', 'react-docgen', 'bin', 'react-docgen.js')
+var tmpOutput = path.join(__dirname, 'lib', 'react-doc-gen-output.json')
+var componentsPath
+
+if (fs.existsSync(importJSConfigPath)) {
+  var config = JSON.parse(fs.readFileSync(importJSConfigPath, 'utf-8'))
+  componentsPath = (config.lookupPaths) ? config.lookupPaths.join(" ") : null
+} else {
+  console.log('no .importjs.json config file found.')
+}
+
+if (argv.src) {
+  componentsPath = argv.src
+}
+
+if(!componentsPath) {
+  console.log('no .importjs.json config file found. component --src passed in')
+  process.exit(-1);
+}
+// Do docgen from paths provided and then generate autocompletions
+exec(`${docGenPath} ${componentsPath} -o ${tmpOutput}`, {cwd: cwd}, function (error, stdout, stderr) {
+  console.log('React Doc Gen Finished. Build Tokens')
+  generateCompletions()
+});
---
+++ parse-docgen.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+var argv = require('yargs').argv;
+var fs = require('fs')
+var path = require('path')
+var cwd = process.cwd()
+/**
+ * takes in react-doc-gen json and generates Atom autocomplete json
+ * @return {json} data used by provider.js autocomplete+
+ */
+
+var output = path.join(__dirname, 'lib', 'react-doc-gen-output.json')
+
+function generateCompletions() {
+  var tokenFile = JSON.parse(fs.readFileSync(output, 'utf-8'))
+  var completions = {
+    components: {}
+  }
+  for (var key in tokenFile) {
+    var componentName = path.basename(key, '.js')
+    if(componentName === 'index') {
+      var componentName = capitalizeFirstLetter(path.basename(path.dirname(key)))
+      console.log('Component name is index. Change to (D)irectory name', componentName)
+    }
+
+    if(!completions.components[componentName]) {
+      completions.components[componentName] = {}
+    } else {
+      console.log('component name collision', componentName)
+    }
+
+    if(tokenFile[key].description) {
+      // set description of component
+      completions.components[componentName].description = 'test'
+    }
+
+    if(tokenFile[key].props) {
+      // set props of component
+      completions.components[componentName].attributes = Object.keys(tokenFile[key].props)
+      completions.components[componentName].props = tokenFile[key].props
+    }
+
+  }
+
+  var dest = path.join(cwd, 'completions.json')
+  fs.writeFile(dest, JSON.stringify(completions, null, 2), function (err) {
+    if (err) {
+      return console.log(err)
+    }
+    console.log('autocomplete tokens generated')
+  })
+}
+
+module.exports = generateCompletions
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
